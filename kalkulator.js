21602eabc4dab8b7c9d1a46a4ac48ffdae43a96a
Juhász Balázs <71382561+juhaszbalazs2005@users.noreply.github.com>
2022-04-06 10:09:37 +0200

    Delete kalkulator.js

--- kalkulator.js
+++
@@ -1,7 +0,0 @@
-function kalkulatorjs() {
-    var x = document.getElementById("kalkulatorjshtml");
-    var text = "";
-    var i;
-    text += x.elements[1].value + "költségvetése" + x.elements[2].value*x.elements[3] + "<br>";
-    document.getElementById("kalkulatorszoveg").innerHTML = text;
-  }
